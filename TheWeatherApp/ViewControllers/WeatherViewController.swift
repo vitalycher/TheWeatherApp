cb0062f866836943c2c865600f54e1f4e3b73beb
Vitaly Chernysh <v.chernish@111minutes.com>
2018-07-04 23:42:23 +0300

    Added CollectionView. Working on binding Forecast to CollectionViewCells.

--- TheWeatherApp/ViewControllers/WeatherViewController.swift
+++ TheWeatherApp/ViewControllers/WeatherViewController.swift
@@ -14,13 +14,18 @@ import RxSwift
 class WeatherViewController: UIViewController {
 
     @IBOutlet private weak var slider: UISlider!
-
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     private let disposeBag = DisposeBag()
     private let locationService = LocationService()
     private var forecastMenu = ForecastMenu()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "GeneralForecastCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "GeneralForecastCollectionViewCell")
 
         locationService.delegate = self
         locationService.authorizeAndFetchCoordinates()
@@ -29,7 +34,9 @@ class WeatherViewController: UIViewController {
 
         slider.rx.value.subscribe(onNext: { sliderValue in
             self.slider.value = round(sliderValue)
-            self.forecastMenu.setForecastDaysAmount(with: Int(sliderValue))
+//            print(round(sliderValue))
+//            print(sliderValue)
+            self.forecastMenu.setForecastDaysAmount(with: Int(round(sliderValue)))
         }).disposed(by: disposeBag)
     }
 
@@ -37,7 +44,7 @@ class WeatherViewController: UIViewController {
         forecastMenu.performInitialFetching(withCoordinate: coordinate)
         
         forecastMenu.forecast.skip(1).subscribe { forecast in
-            print(forecast.element?.city)
+            self.collectionView.reloadData()
         }.disposed(by: disposeBag)
         
         forecastMenu.didRecieveError.subscribe { errorEvent in
@@ -57,10 +64,44 @@ class WeatherViewController: UIViewController {
 }
 
 extension WeatherViewController: LocationServiceDelegate {
+
     func locationService(_ service: LocationService, didFetchCoordinate coordinate: CLLocationCoordinate2D) {
         configureForecastMenu(withCoordinate: coordinate)
     }
+    
     func locationServiceDidRejectUpdatingCoordinate(service: LocationService) {
         configureForecastMenu(withCoordinate: CLLocationCoordinate2D.defaultCoordinate)
+    }
+
+}
+
+extension WeatherViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return forecastMenu.currentForecastDaysAmount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralForecastCollectionViewCell", for: indexPath) as? GeneralForecastCollectionViewCell {
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+
+}
+
+extension WeatherViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension WeatherViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width * 0.4, height: collectionView.frame.height)
     }
 }
---
+++ TheWeatherApp/Views/GeneralForecastCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralForecastCollectionViewCell.swift
+//  TheWeatherApp
+//
+//  Created by Vitaly Chernysh on 04.07.2018.
+//  Copyright © 2018 Vitaly Chernysh. All rights reserved.
+//
+
+import UIKit
+
+class GeneralForecastCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
